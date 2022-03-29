b5831e9e85d54433007a7914e7295c5315edb265
Erick18350498 <isc18350498@gmail.com>
2022-03-28 23:34:16 -0600

    SEGUNDO COMMIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 
 function App() {
+  //todos: presentar el concepto de state
+  let varprueba = "titulo"
+  const handleChangePrueba = (event) => {
+    //console.log (event.target.name, event.target.value);
+    varprueba = event.target.value
+  }
   return (
     <div className="App">
       <h1>notas</h1>
+      <label htmlFor="pruebaID">Input de prueba</label>
+      <input
+      
+      id="pruebaID"
+      name="prueba"
+      type="text"
+      onChange={handleChangePrueba}
+      value={varprueba}
+      />
     </div>
   );
 }
