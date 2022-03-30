921e749f0606a94e59df2c2af9ed7f62467bfb1a
Erick18350498 <isc18350498@gmail.com>
2022-03-29 23:50:42 -0600

    Integracion de los estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,68 @@
+import { useState } from "react";
 
 function App() {
-  //todos: presentar el concepto de state
-  let varprueba = "titulo"
-  const handleChangePrueba = (event) => {
-    //console.log (event.target.name, event.target.value);
-    varprueba = event.target.value
-  }
+//TODOS: presentar el concepto de state 
+// Hooks 
+const  [tituloState, setTituloState]= useState("");
+const  [fechaState, setFechaState]= useState("");
+const  [notaState, setNotaState]= useState("");
+//let varprueba = "titulo"
+
+const handleChangeTitulo = (event) => {
+   setTituloState(event.target.value);
+  //console.log (event.target.name, event.target.value);
+  //varprueba = event.target.value
+
+}
+
+const handleChangeFecha = (event) => {
+  setFechaState(event.target.value);
+ //console.log (event.target.name, event.target.value);
+ //varprueba = event.target.value
+
+}
+const handleChangeNota = (event) => {
+  setNotaState(event.target.value);
+ //console.log (event.target.name, event.target.value);
+ //varprueba = event.target.value
+
+}
   return (
     <div className="App">
-      <h1>notas</h1>
-      <label htmlFor="pruebaID">Input de prueba</label>
-      <input
-      
-      id="pruebaID"
-      name="prueba"
-      type="text"
-      onChange={handleChangePrueba}
-      value={varprueba}
-      />
+    <h3 style={{textAlign:center}} >Formulario</h3>
+    <label htmlFor="Titulo"> Titulo</label>
+    <br></br>
+    <input 
+    id="Titulo" 
+    name="Titulo" 
+    type="text" 
+    onChange={handleChangeTitulo}
+    value={tituloState}
+    />
+
+    <br>
+    </br>
+
+<label htmlFor="Fecha"> Fecha</label>
+    <input 
+    id="Fecha" 
+    name="Fecha" 
+    type="text" 
+    onChange={handleChangeFecha}
+    value={fechaState}
+    />
+    <br>
+    </br>
+
+<label htmlFor="Nota"> Nota</label>
+    <input 
+    id="Nota" 
+    name="Nota" 
+    type="text" 
+    onChange={handleChangeNota}
+    value={notaState}
+    />
+
     </div>
   );
 }
